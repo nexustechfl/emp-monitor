71da1e11344efd3c2f064111e4ae162e82a3f357
indianbill007 <indianbill007@gmail.com>
2026-03-26 01:40:45 +0530

    security: Move Atlassian OAuth secrets to env vars, remove token logging, fix .gitignore

--- Backend/admin/src/utils/passport/PassportSetup.js
+++ Backend/admin/src/utils/passport/PassportSetup.js
@@ -15,33 +15,12 @@ passport.deserializeUser((obj, done) =>
 );
 
 passport.use(new AtlassianStrategy({
-    clientID: 'bkrPFF0qRqI81u1D9O9FxdpsXPFOfxy6',
-    clientSecret: '8fyCqRJ2-I_RgFfXMDa3SZB7BDo7ZMEGMP7r2UxVUggsgbwJarzMlm5CA9jzNBio',
+    clientID: process.env.ATLASSIAN_CLIENT_ID,
+    clientSecret: process.env.ATLASSIAN_CLIENT_SECRET,
     callbackURL: 'https://35a957de.ngrok.io/auth/atlassian/callback',
     scope: 'offline_access read:jira-user read:jira-work',
 },
     (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken ==> ', accessToken)
-        console.log('refreshToken ==> ', refreshToken)
-        console.log(profile._json)
         done(null, profile);
-
-        // check if user already exists in our own db
-        // User.findOne({ googleId: profile.id }).then((currentUser) => {
-        //     if (currentUser) {
-        //         // already have this user
-        //         console.log('user is: ', currentUser);
-        //         done(null, currentUser); // Next Stage is serialize => Stuff data in cookie
-        //     } else {
-        //         // if not, create user in our db
-        //         new User({
-        //             googleId: profile.id,
-        //             username: profile.displayName
-        //         }).save().then((newUser) => {
-        //             console.log('created new user: ', newUser);
-        //             done(null, newUser); // Next Stage is serialize => Stuff data in cookie
-        //         });
-        //     }
-        // });
     }
 ));
